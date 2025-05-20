4d34986d4bf67403649f4c23c58d8b26f19df729
ms0ur <maksimsuslov7142@gmail.com>
2025-05-20 14:08:53 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/content',
+    '@nuxtjs/turnstile',
+    '@nuxt/icon'
+  ]
+})
